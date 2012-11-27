21712a4ce29d678cf29791266826b8fbac1bf0d9
und3r5core <emanuele.perricelli@gmail.com>
2012-11-27 18:27:32 +0100

    a slight change to see how commit works

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,10 +2,9 @@
 public class HelloWorld {
 
 	/**
-	 * @param args
+	 * Test class to test GitHub commit
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World!");
 
